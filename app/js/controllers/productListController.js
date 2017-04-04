cd7ae415de4b0b11f74e5d54b3d46820cf1ef220
pradeep <pradeep.p@happiestminds.com>
2017-04-04 12:14:55 +0530

    Enabling auto refresh and passing param

--- app/js/controllers/productListController.js
+++ app/js/controllers/productListController.js
@@ -1,6 +1,6 @@
 angular.module ('trustCafeApp')
 .controller ('ProductListCtrl', ['$scope', '$translate', '$http', '$interval', 'NgTableParams', function ($scope, $translate, $http, $interval, NgTableParams) {
-	$scope.REFRESH_TIME = 10000
+	$scope.REFRESH_TIME = 30000
 
 	function getData() {
 		$http({
@@ -9,24 +9,26 @@ angular.module ('trustCafeApp')
 		}).then(function successCallback(response) {
 			var data = response.data
 			$scope.tableParams = new NgTableParams({}, { dataset: data});
+			console.log("Table updated")
 		}, function errorCallback(error) {
 			console.log (error)
 		});
-	}
+	};
 
-	$scope.playPause = function () {
+	$scope.playPause = function (id) {
+		console.log("Instance to start/stop ->"+id)
 		$http({
 		  method: 'GET',
-		  url: '/api/playPause'
+		  url: '/api/playPause/'+id
 		}).then(function successCallback(response) {
-			console.log("Resfrshing table")
+			console.log("Resfreshing table")
 			getData();
 		}, function errorCallback(error) {
 			console.log (error)
 		});
-	}
+	};
 
-	// $interval(getData, $scope.REFRESH_TIME);
+	$interval(getData, $scope.REFRESH_TIME);
 	getData();
 
 }])
